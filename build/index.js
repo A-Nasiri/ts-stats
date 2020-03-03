4ead6eaf2a671d058a1797ad7a194f1236fa93fb
A.Nasiri <50527356+A-Nasiri@users.noreply.github.com>
2020-03-03 00:36:49 +0000

    Replacing MatchResult object identifiers with enums

--- build/index.js
+++ build/index.js
@@ -1,2 +1,31 @@
 "use strict";
-console.log('hi there');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+var matches = fs_1.default
+    .readFileSync('original.csv', {
+    encoding: 'utf-8'
+})
+    .split('\n')
+    .map(function (row) {
+    return row.split(',');
+});
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+var manUtdWins = 0;
+for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
+    var match = matches_1[_i];
+    if (match[0] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUtdWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUtdWins++;
+    }
+}
+console.log("Man United has won " + manUtdWins + " times!");
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('hi there');
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('original.csv', {
+    encoding: 'utf-8'
+  })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
+
+let manUtdWins = 0;
+
+for (let match of matches) {
+  if (match[0] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUtdWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUtdWins++;
+  }
+}
+
+console.log(`Man United has won ${manUtdWins} times!`);
