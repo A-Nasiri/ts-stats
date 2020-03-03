13c79b3183043d3d48fc395ac26d88a2a4455314
A.Nasiri <50527356+A-Nasiri@users.noreply.github.com>
2020-03-03 20:37:01 +0000

    CsvFileReader Class

--- build/index.js
+++ build/index.js
@@ -1,17 +1,8 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = __importDefault(require("fs"));
-var matches = fs_1.default
-    .readFileSync('original.csv', {
-    encoding: 'utf-8'
-})
-    .split('\n')
-    .map(function (row) {
-    return row.split(',');
-});
+var CsvFileReader_1 = require("./CsvFileReader");
+var reader = new CsvFileReader_1.CsvFileReader('original.csv');
+reader.read();
 var MatchResult;
 (function (MatchResult) {
     MatchResult["HomeWin"] = "H";
@@ -19,8 +10,8 @@ var MatchResult;
     MatchResult["Draw"] = "D";
 })(MatchResult || (MatchResult = {}));
 var manUtdWins = 0;
-for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
-    var match = matches_1[_i];
+for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
+    var match = _a[_i];
     if (match[0] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUtdWins++;
     }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs
-  .readFileSync('original.csv', {
-    encoding: 'utf-8'
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+const reader = new CsvFileReader('original.csv');
+reader.read();
 
 enum MatchResult {
   HomeWin = 'H',
@@ -17,7 +11,7 @@ enum MatchResult {
 
 let manUtdWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[0] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUtdWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
