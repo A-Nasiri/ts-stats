86550f60bbc7fe1dd661f34fd5f6e664a5ea482c
A.Nasiri <50527356+A-Nasiri@users.noreply.github.com>
2020-10-27 19:24:50 +0000

    Composition-based approach: Composing all classes in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResult';
 import { CsvFileReader } from './CsvFileReader';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { Summary } from './Summary';
 
 //Create an object that satisfies the "DataReader" interface
 const csvFileReader = new CsvFileReader('football.csv');
@@ -11,3 +13,9 @@ const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
 
+const summary = new Summary(
+  new WinsAnalysis('Man United'),
+  new ConsoleReport()
+);
+
+summary.buildAndPrintReport(matchReader.matches);
--- src/reportTargets/ConsoleReport.ts
+++ src/reportTargets/ConsoleReport.ts
@@ -1,6 +1,4 @@
 import { OutputTarget } from '../Summary';
-import { MatchData } from '../MatchData';
-import { MatchResult } from '../MatchResult';
 
 export class ConsoleReport implements OutputTarget {
     print(report: string): void {
